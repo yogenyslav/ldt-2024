a7d6745e4aea195827c7d6df2aa19cc5798c9008
Evgeny Gurov <evgenygurov9@mail.ru>
2024-06-16 12:43:39 +0300

    fix(bot): markdown parsing param

--- chat/internal/bot/auth/handler/auth.go
+++ chat/internal/bot/auth/handler/auth.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+
 	"github.com/yogenyslav/ldt-2024/chat/pkg"
 	tele "gopkg.in/telebot.v3"
 )
@@ -11,7 +12,7 @@ func (h *Handler) Auth(c tele.Context) error {
 	_, span := h.tracer.Start(pkg.GetTraceCtx(c), "Handler.Auth.Send")
 	defer span.End()
 
-	url := fmt.Sprintf("Авторизация по http://localhost:5173?tg_id=%d", c.Sender().ID)
+	url := fmt.Sprintf("Авторизация по [ссылке](http://localhost:5173?tg_id=%d)", c.Sender().ID)
 
 	return c.Send(url, &tele.SendOptions{
 		DisableWebPagePreview: true,
