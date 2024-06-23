9d8047462b870648098e52c180b3e76b8b118f8b
Evgeny Gurov <evgenygurov9@mail.ru>
2024-06-23 14:49:04 +0300

    fix(organization): add organization_id for test admin

--- migrations/20240622134242_user_organization_org_id.sql
+++ migrations/20240622134242_user_organization_org_id.sql
@@ -3,7 +3,7 @@
 alter table adm.organization
     drop column s3_bucket;
 alter table adm.user_organization
-    add column organization_id bigint,
+    add column organization_id bigint not null default 0,
     drop column organization;
 create index user_organization_organization_id_idx on adm.user_organization (organization_id);
 -- +goose StatementEnd
